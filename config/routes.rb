1b907c7b090655dd82761eeb8b50765ce0925ac7
T.Tsujii <ttakehisabt@gmail.com>
2019-12-13 16:49:24 +0900

    Completed the chat function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  mount ActionCable.server => '/cable'
   devise_for :users
   root 'rooms#show'
   resources :messages, only: :create
+  get '/show_additionally', to: 'rooms#show_additionally'
 end
